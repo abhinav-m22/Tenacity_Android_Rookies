7d690d58f672cfe67c3f0ad65c706876662ca564
Manas Dalvi <dalvimanas33@gmail.com>
2023-02-11 18:38:39 +0530

    Customer Ui 3

--- lib/features/customer/provider_food_profile.dart
+++ lib/features/customer/provider_food_profile.dart
@@ -98,94 +98,96 @@ class _ListItemPageState extends State<ListItemPage> {
             ),
           )),
       drawer: Drawer(child: CustomDrawer()),
-      body: Column(
-        children: [
-          Container(
-            // color: Colors.blueGrey,
-            padding: const EdgeInsets.all(20),
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height * 0.2,
-            // color: Colors.amber,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      // color: Colors.amber,
-                      child: Text('Restaurant Name',
-                          overflow: TextOverflow.clip,
-                          style: GoogleFonts.oswald(
-                              color: Colors.black,
-                              fontSize: 25,
-                              fontWeight: FontWeight.w300)),
-                    ),
-                    Container(
-                      width: MediaQuery.of(context).size.width * 0.5,
-                      // color: Colors.red,
-                      child: Text('This is description of the restaurant',
-                          overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.oswald(
-                              color: Colors.black, fontSize: 15)),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 15, right: 20),
-                  child: CircleAvatar(
-                    backgroundImage: AssetImage("assets/images/a.avif"),
-                    radius: 40,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              // color: Colors.blueGrey,
+              padding: const EdgeInsets.all(20),
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height * 0.2,
+              // color: Colors.amber,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        // color: Colors.amber,
+                        child: Text('Restaurant Name',
+                            overflow: TextOverflow.clip,
+                            style: GoogleFonts.oswald(
+                                color: Colors.black,
+                                fontSize: 25,
+                                fontWeight: FontWeight.w300)),
+                      ),
+                      Container(
+                        width: MediaQuery.of(context).size.width * 0.5,
+                        // color: Colors.red,
+                        child: Text('This is description of the restaurant',
+                            overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.oswald(
+                                color: Colors.black, fontSize: 15)),
+                      ),
+                    ],
                   ),
-                ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 15, right: 20),
+                    child: CircleAvatar(
+                      backgroundImage: AssetImage("assets/images/a.avif"),
+                      radius: 40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Container(
+                    // padding: const EdgeInsets.only(left),
+                    margin: const EdgeInsets.only(left: 100,right:100),
+                    height: 40,
+                    // width: MediaQuery.of(context).size.width*0.3,
+                    decoration: BoxDecoration(
+                        border: Border.all(color: Colors.white, width: 0.0),
+                        borderRadius: BorderRadius.circular(50),
+                        color: Colors.amber
+                        ),
+                    child: OutlinedButton(
+                      style: ElevatedButton.styleFrom(
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(50)),
+                          primary: Colors.black12.withOpacity(0.03)),
+                      onPressed: () {},
+                      child: Text('Donate',
+                            // overflow: TextOverflow.ellipsis,
+                            style: GoogleFonts.oswald(
+                                color: Colors.white, fontSize: 20)),
+                    ),
+                  ),
+                )
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Expanded(
-                child: Container(
-                  // padding: const EdgeInsets.only(left),
-                  margin: const EdgeInsets.only(left: 100,right:100),
-                  height: 40,
-                  // width: MediaQuery.of(context).size.width*0.3,
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 0.0),
-                      borderRadius: BorderRadius.circular(50),
-                      color: Colors.amber
-                      ),
-                  child: OutlinedButton(
-                    style: ElevatedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(50)),
-                        primary: Colors.black12.withOpacity(0.03)),
-                    onPressed: () {},
-                    child: Text('Donate',
-                          // overflow: TextOverflow.ellipsis,
-                          style: GoogleFonts.oswald(
-                              color: Colors.white, fontSize: 20)),
-                  ),
-                ),
-              )
-            ],
-          ),
-          SizedBox(height: 50,),
-          ListView.builder(
-                scrollDirection: Axis.vertical,
-                shrinkWrap: true,
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: FoodItem(),
-                  );
-                 }),
-          
-        ],
+            SizedBox(height: 40,),
+            ListView.builder(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: 10,
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: FoodItem(),
+                    );
+                   }),
+            
+          ],
+        ),
       ),
     );
   }
